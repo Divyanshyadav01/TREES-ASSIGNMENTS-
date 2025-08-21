ab819aa7edeb275d8b73ee8ac6bd7cbd16bdd30c
Divyanshyadav01 <divyanshyaduvanshi0@gmail.com>
2025-08-21 21:54:25 +0530

    Create DeleteBST.java

---
+++ 2025-08-21/DeleteBST.java
@@ -0,0 +1,19 @@
+class Solution{
+    public TreeNode deleteNode(TreeNode root,int key){
+        if(root==null)return null;
+        if(key<root.val)root.left=deleteNode(root.left,key);
+        else if(key>root.val)root.right=deleteNode(root.right,key);
+        else{
+            if(root.left==null)return root.right;
+            else if(root.right==null)return root.left;
+            TreeNode temp=findMin(root.right);
+            root.val=temp.val;
+            root.right=deleteNode(root.right,temp.val);
+        }
+        return root;
+    }
+    private TreeNode findMin(TreeNode node){
+        while(node.left!=null)node=node.left;
+        return node;
+    }
+}
